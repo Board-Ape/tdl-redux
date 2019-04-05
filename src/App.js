9b6e6bd6f4a0103a7ccc630c4a52415a2568d602
Sam Singer <samuel.singer1@gmail.com>
2019-04-04 17:07:19 -0700

    Delete todo

--- src/App.js
+++ src/App.js
@@ -1,13 +1,31 @@
 import React, { Component } from 'react';
 import './App.css';
+import Todos from './Todos';
 
 class App extends Component {
+  state = {
+    todos: [
+      {id: 1, content: 'Buy some milk'},
+      {id: 2, content: 'Watch movies with friends'}
+    ]
+  }
+
+  deleteTodo = (id) => {
+    console.log(id)
+    const todos = this.state.todos.filter(todo => todo.id !== id)
+
+    this.setState({
+      todos
+    })
+  }
+
   render() {
     return (
-      <div className="App">
+      <div className="todo-app contaner">
         <header className="App-header">
-          <h1>Learning Redux</h1>
+          <h1 className="center blue-text">Todo's</h1>
         </header>
+        <Todos todos={this.state.todos} deleteTodo={this.deleteTodo} />
       </div>
     );
   }
